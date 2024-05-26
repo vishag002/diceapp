48a196e2813ce084bedc873bb7044c6e26c5c6bd
vishag002 <vvishag477@gmail.com>
2024-05-26 20:33:14 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
-        debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: true,
         // home: HomeScreen(),
         home: Homepage(),
       ),
